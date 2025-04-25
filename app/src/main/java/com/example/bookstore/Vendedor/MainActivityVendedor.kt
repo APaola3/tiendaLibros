bdf84cee053f0a3fcd04d7df5a4f0ec669dd1651
EdwCami <122037465+EdwCami@users.noreply.github.com>
2025-04-25 11:25:16 -0500

    proyecto

---
+++ app/src/main/java/com/example/bookstore/Vendedor/MainActivityVendedor.kt
@@ -0,0 +1,16 @@
+package com.example.bookstore.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bookstore.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_vendedor)
+
+    }
+}

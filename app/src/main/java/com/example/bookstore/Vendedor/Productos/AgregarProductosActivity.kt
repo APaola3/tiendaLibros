656ab26ff973271a71ae6883f2e62f2728914d1e
EdwCami <paola.ross.031@gmail.com>
2025-05-30 18:44:39 -0500

    Agregar producto a la BD

--- app/src/main/java/com/example/bookstore/Vendedor/Productos/AgregarProductosActivity.kt
+++ app/src/main/java/com/example/bookstore/Vendedor/Productos/AgregarProductosActivity.kt
@@ -2,8 +2,10 @@ package com.example.bookstore.Vendedor.Productos
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
@@ -20,6 +22,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class AgregarProductosActivity : AppCompatActivity() {
 
@@ -31,12 +34,32 @@ class AgregarProductosActivity : AppCompatActivity() {
 
     private lateinit var  categoriasArrayList: ArrayList<ModeloCategoria>
 
+    private lateinit var progressDialog : ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarProductosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         cargarCategorias()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.etPrecioConDescuentoP.visibility = View.GONE
+        binding.etNotaDescuentoP.visibility = View.GONE
+
+        binding.descuentoSwitch.setOnCheckedChangeListener {buttonView, isCheked->
+            if (isCheked){
+                //swith está habilitado
+                binding.etPrecioConDescuentoP.visibility = View.VISIBLE
+                binding.etNotaDescuentoP.visibility = View.VISIBLE
+            }else{
+                binding.etPrecioConDescuentoP.visibility = View.GONE
+                binding.etNotaDescuentoP.visibility = View.GONE
+            }
+        }
 
         imagenSelecArrayList = ArrayList()
 
@@ -54,8 +77,126 @@ class AgregarProductosActivity : AppCompatActivity() {
         cargarImagenes()
     }
 
+    private var nombreP = ""
+    private var descripcionP = ""
+    private var categoriaP = ""
+    private var precioP = ""
+    private var descuentoHab = false
+    private var precioDescP = ""
+    private var notaDescP = ""
     private fun validarInfo() {
-        
+        nombreP = binding.etNombresP.text.toString().trim()
+        descripcionP = binding.etDescripcionP.text.toString().trim()
+        categoriaP = binding.Categoria.text.toString().trim()
+        precioP = binding.etPrecioP.text.toString().trim()
+        descuentoHab = binding.descuentoSwitch.isChecked
+
+        if (nombreP.isEmpty()){
+            binding.etNombresP.error = "Ingrese nombre"
+            binding.etNombresP.requestFocus()
+        }
+        else if (descripcionP.isEmpty()){
+            binding.etDescripcionP.error = "Indrese descripción"
+            binding.etDescripcionP.requestFocus()
+        }
+        else if (categoriaP.isEmpty()){
+            binding.Categoria.error = "Seleccione una categoría"
+            binding.etPrecioP.requestFocus()
+        }
+        else if (precioP.isEmpty()){
+            binding.etPrecioP.error = "Ingrese precio"
+            binding.etPrecioP.requestFocus()
+        }
+        else if (imagenUri == null){
+            Toast.makeText(this, "Seleccione al menos una imagen", Toast.LENGTH_SHORT).show()
+        }else{
+            //descuentoHab = true
+            if (descuentoHab){
+                precioDescP = binding.etPrecioConDescuentoP.text.toString().trim()
+                notaDescP = binding.etNotaDescuentoP.text.toString().trim()
+                if (precioDescP.isEmpty()){
+                    binding.etPrecioConDescuentoP.error = "Ingrese precio con desc."
+                    binding.etPrecioConDescuentoP.requestFocus()
+                }else if (notaDescP.isEmpty()){
+                    binding.etNotaDescuentoP.text.toString().trim()
+                    binding.etNotaDescuentoP.requestFocus()
+                }else{
+                    agregarProducto()
+                }
+            }
+        }
+    }
+
+    private fun agregarProducto(){
+        progressDialog.setMessage("Agregando producto")
+        progressDialog.show()
+
+        var ref = FirebaseDatabase.getInstance().getReference("Productos")
+        val keyId = ref.push().key
+
+        val hasMap = HashMap<String, Any>()
+        hasMap["is"] = "${keyId}"
+        hasMap["nombre"] = "${nombreP}"
+        hasMap["descripcion"] = "${descripcionP}"
+        hasMap["categoria"] = "${categoriaP}"
+        hasMap["precioDesc"] = "${precioDescP}"
+        hasMap["notaDesc"] = "${notaDescP}"
+
+        ref.child(keyId!!)
+            .setValue(hasMap)
+            .addOnSuccessListener {
+                subirImgStorage(keyId)
+            }
+            .addOnFailureListener {e->
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun subirImgStorage(KeyId: String) {
+        for (i in imagenSelecArrayList.indices){
+            val modeloImagenSel = imagenSelecArrayList[i]
+            val nombreImagen = modeloImagenSel.id
+            val rutaImagen = "Productos/$nombreImagen"
+
+            val storageRef = FirebaseStorage.getInstance().getReference(rutaImagen)
+            storageRef.putFile(modeloImagenSel.imageUri!!)
+                .addOnSuccessListener {taskSnapshot->
+                    val uriTask = taskSnapshot.storage.downloadUrl
+                    while (!uriTask.isSuccessful);
+                    val urlImgCargada = uriTask.result
+
+                    if (uriTask.isSuccessful){
+                        val hasMap = HashMap<String, Any>()
+                        hasMap["id"] = "${modeloImagenSel.id}"
+                        hasMap["imagenUrl"] = "${urlImgCargada}"
+
+                        val ref = FirebaseDatabase.getInstance().getReference("Productos")
+                        ref.child(KeyId).child("Imagenes")
+                            .child(nombreImagen)
+                            .updateChildren(hasMap)
+                        progressDialog.dismiss()
+                        Toast.makeText(this, "Se agregó el producto", Toast.LENGTH_SHORT).show()
+                        limpiarCampos()
+                    }
+
+                }
+                .addOnFailureListener {e->
+                    progressDialog.dismiss()
+                    Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun limpiarCampos() {
+        imagenSelecArrayList.clear()
+        adaptadorImagenSel.notifyDataSetChanged()
+        binding.etNombresP.setText("")
+        binding.etDescripcionP.setText("")
+        binding.etPrecioP.setText("")
+        binding.Categoria.setText("")
+        binding.descuentoSwitch.isChecked = false
+        binding.etPrecioConDescuentoP.setText("")
+        binding.etNotaDescuentoP.setText("")
     }
 
     private fun cargarCategorias() {
